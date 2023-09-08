965dea53bd914e395b77a44e7a486524458c20e5
paichiwo <lzerucha@gmail.com>
2023-09-08 23:33:19 +0100

    Test functionality

--- my_main.py
+++ my_main.py
@@ -2,22 +2,32 @@ import customtkinter
 from faker import Faker
 from ctktableview.my_tableview import CTkTableView
 
+
+def fakeit():
+
+    data = []
+    f = Faker()
+    for n in range(10):
+        data.append([f.user_name(), f.password(), f.url()])
+    print(data)
+    table.insert_rows(data)
+
+
 if __name__ == "__main__":
     root = customtkinter.CTk()
     root.title("CustomTkinter Table")
     root.geometry("450x400")
 
-    table = CTkTableView(root)
+    btn = customtkinter.CTkButton(root, text="Fake it", command=fakeit)
+    btn.pack()
+
+    values = [
+        ['test_user0', 'test_pass0', 'test_url0'],
+        ['test_user1', 'test_pass1', 'test_url1']
+    ]
+
+    table = CTkTableView(root, values=values)
     table.pack(expand=True, fill='both')
 
-    data = []
-    f = Faker()
-    for n in range(100):
-        name = f.name()
-        age = f.random.randint(18, 70)
-        email = f.email()
-        data.append([name, age, email])
-
-    # table.update_table(data)
-
     root.mainloop()
+
