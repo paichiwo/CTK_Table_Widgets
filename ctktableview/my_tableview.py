3651d66168c59c2ead56c5986eb6adcb9a32544a
paichiwo <lzerucha@gmail.com>
2023-09-08 23:35:25 +0100

    Create clear, insert_row and update_view methods in CTkTableView.

--- ctktableview/my_tableview.py
+++ ctktableview/my_tableview.py
@@ -1,29 +1,35 @@
-
 import customtkinter as ctk
 
 
 class CTkTableView(ctk.CTkScrollableFrame):
-    def __init__(self, master, *args, **kwargs):
+    def __init__(self, master, *args, values: list, **kwargs):
         super().__init__(master, *args, **kwargs)
 
-        self.master = master
-        self.frame = Frame(self, "username", "password", "website", fg_color='black')
-        self.frame.pack(fill='both', ipady=10)
+        self.values = values
+        self.frames = []
 
-        self.master = master
-        self.frame = Frame(self, "username", "password", "website", fg_color='black')
-        self.frame.pack(fill='both', ipady=10, pady=10)
+        self.update_view()
 
-        self.master = master
-        self.frame = Frame(self, "username", "password", "website", fg_color='black')
-        self.frame.pack(fill='both', ipady=10)
+    def clear(self):
+        for frame in self.frames:
+            frame.destroy()
+        self.frames = []
+
+    def insert_rows(self, data):
+        self.clear()
+        for item in data:
+            frame = Frame(self, *item, fg_color='black')
+            frame.pack(fill='both', ipady=10, pady=5)
+            self.frames.append(frame)
+
+    def update_view(self):
+        self.clear()
+        self.insert_rows(self.values)
 
 
 class Frame(ctk.CTkFrame):
     def __init__(self, master, username, password, website, *args, **kwargs):
         super().__init__(master, *args, **kwargs)
-
-        self.master = master
 
         self.username = username
         self.password = password
@@ -36,32 +42,33 @@ class Frame(ctk.CTkFrame):
         self.website_label = None
         self.website_entry = None
 
-        self.columnconfigure(0, weight=1)
-        self.columnconfigure(1, weight=1)
-
         self.gui()
-        self.insert_data()
 
     def gui(self):
 
+        self.columnconfigure(0, weight=1)
+        self.columnconfigure(1, weight=1)
+
+        # --- USERNAME ---
         self.username_label = ctk.CTkLabel(self, text="Username")
         self.username_label.grid(row=0, column=0, sticky='w', padx=20, pady=10)
+
         self.username_entry = ctk.CTkEntry(self)
         self.username_entry.grid(row=1, column=0, sticky='ew', padx=20)
+        self.username_entry.insert(0, self.username)
 
+        # --- PASSWORD ---
         self.password_label = ctk.CTkLabel(self, text="Password")
         self.password_label.grid(row=2, column=0, sticky='w', padx=20, pady=10)
+
         self.password_entry = ctk.CTkEntry(self)
         self.password_entry.grid(row=3, column=0, sticky='ew', padx=20)
+        self.password_entry.insert(0, self.password)
 
+        # --- WEBSITE ---
         self.website_label = ctk.CTkLabel(self, text="Website")
         self.website_label.grid(row=0, column=1, sticky='w', padx=20, pady=10)
+
         self.website_entry = ctk.CTkEntry(self)
         self.website_entry.grid(row=1, column=1, sticky='ew', padx=20)
-
-    def insert_data(self):
-        self.username_entry.insert(0, self.username)
-        self.password_entry.insert(0, self.password)
         self.website_entry.insert(0, self.website)
-
-
